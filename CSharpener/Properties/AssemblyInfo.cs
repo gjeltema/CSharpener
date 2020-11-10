737a0acd22e9b312c15ab32ac1f2dbcc8f7f48a4
Craig Gjeltema <gjeltemac@outlook.com>
2020-11-10 14:04:35 +0100

    Formatted code files.

--- CSharpener/Properties/AssemblyInfo.cs
+++ CSharpener/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿// -----------------------------------------------------------------------
+// AssemblyInfo.cs Copyright 2020 Craig Gjeltema
+// -----------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
